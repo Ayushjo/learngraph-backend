b919a868084fb106d80ffc8a8fdab3812c0b95e2
AyushJo <ayushs24101@iiitnr.edu.in>
2026-03-20 10:29:32 +0530

    Made Changes to Prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { env } from "../config/env";
+
+const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -7,6 +11,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
